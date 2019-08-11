0e21b8e41dc3b05fd12f470051211308d66e1ead
gusbeane <abeane@sas.upenn.edu>
2019-08-11 19:31:26 -0400

    Update run_halo_gen.py

--- scripts/run_halo_gen.py
+++ scripts/run_halo_gen.py
@@ -10,20 +10,21 @@ basedir = '/Users/abeane/scratch/ztran_OIII_sims/v1.2/256Mpch/256'
 fid_dir = basedir + '/fid'
 cold_dir = basedir + '/cold'
 hot_dir = basedir + '/hot'
-dir_list = [hot_dir, fid_dir, cold_dir]
+#dir_list = [hot_dir, fid_dir, cold_dir]
+dir_list = [fid_dir]
 
-Mcut_list = [1E9, 1E10, 1E11]
-Mcut_list_label =['Mcut1e9','Mcut1e10','Mcut1e11']
+Mcut_list = np.logspace(9.7, 11, 10)
+Mcut_list_label =['Mcut'+"{:2.3e}".format(Mcut) for Mcut in Mcut_list]
 
 for directory in dir_list:
     fin_directory = directory + '/Output_files/Halo_lists'
-    fout_directory = directory + '/MyOutput'
+    fout_directory = directory + '/MyOutput/Halo_fields'
 
-    for fin in tqdm(glob.glob(fin_directory+'/halos*')):
+    for fin in tqdm(glob.glob(fin_directory+'/updated_halos*')):
 
         z = float(re.findall('\d?\d\.\d\d', fin)[0])
         zlabel = "{:06.2f}".format(z)
 
         for Mcut,label in zip(Mcut_list,Mcut_list_label):
-            fout = fout_directory+'/halos_z'+zlabel+'_'+label
+            fout = fout_directory+'/updated_halos_z'+zlabel+'_'+label
             gen_halo_catalog(fin, fout, 256, Mcut=Mcut)
