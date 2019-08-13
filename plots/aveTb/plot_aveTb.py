0255b573a285a4ff1f08c9d73a58bd996c28403a
gusbeane <abeane@sas.upenn.edu>
2019-08-13 12:04:27 -0400

    Add xps row to aveTb plot

--- plots/aveTb/plot_aveTb.py
+++ plots/aveTb/plot_aveTb.py
@@ -29,7 +29,7 @@ name_list = [r'\texttt{hot}', r'\texttt{fid}', r'\texttt{cold}']
 fname_list = ['hot', 'fid', 'cold']
 color_list = [tb_c[1], tb_c[-1], tb_c[3]]
 
-fig, ax = plt.subplots(2, 1, figsize=(textwidth, 1.2*columnwidth), sharex=True)
+fig, ax = plt.subplots(3, 1, figsize=(textwidth, 1.5*1.2*columnwidth), sharex=True)
 
 for directory, name, fname, c in zip(dir_list, name_list, fname_list, color_list):
     try:
@@ -37,6 +37,7 @@ for directory, name, fname, c in zip(dir_list, name_list, fname_list, color_list
         nflist = np.load('nflist_'+fname+'.npy')
         aveTb_list = np.load('Tblist_'+fname+'.npy')
         ztran = np.load('ztran_'+fname+'.npy')
+        xps_to_plot = np.load('xps_to_plot_'+fname+'.npy')
     except:
         dTfiles = glob.glob(directory+'/Boxes/delta_T*')
         zlist = np.array([float(re.findall('z\d\d\d\.\d\d', f)[0][1:]) for f in dTfiles])
@@ -51,6 +52,8 @@ for directory, name, fname, c in zip(dir_list, name_list, fname_list, color_list
         this_zlist, klist, xps = read_xps(directory+'/MyOutput/xps*')
         klist, ztran = find_ztran(this_zlist, klist, xps)
         # ztran = ztran[5]
+        xps_to_plot = klist[3]**3 * xps[:,3]/(2.*np.pi**2)
+        print('k=', klist[3])
     
         keys = np.argsort(zlist)
         nflist = nflist[keys]
@@ -60,19 +63,26 @@ for directory, name, fname, c in zip(dir_list, name_list, fname_list, color_list
         np.save('nflist_'+fname+'.npy', nflist)
         np.save('Tblist_'+fname+'.npy', aveTb_list)
         np.save('ztran_'+fname+'.npy', ztran)
+        np.save('xps_to_plot_'+fname+'.npy', xps_to_plot)
 
     ax[0].plot(zlist, 1.-nflist, c=c, label=name)
     ax[1].plot(zlist, aveTb_list, label=name, c=c)
-    ax[1].axvline(ztran[5], c=c, ls='dashed')
+    for x in ax:
+        x.axvline(ztran[3], c=c, ls='dashed')
+    ax[2].plot(zlist, xps_to_plot, label=name, c=c)
 
 ax[0].set_yscale('log')
+ax[2].set_yscale('symlog', linthreshy=0.0001)
 ax[0].set_ylim(0.0001, 1)
 ax[1].set_xlim(26, 6)
 ax[1].set_ylim(-200, 50)
 
 ax[0].set_ylabel(r'$\left<x_{\text{H~\textsc{ii}}}\right>$')
-ax[1].set_xlabel(r'$z$')
-ax[1].set_ylabel(r'$\delta T_b\,[\,\text{mK}\,]$')
+ax[2].set_xlabel(r'$z$')
+ax[1].set_ylabel(r'$\left<\delta T_b\right>\,[\,\text{mK}\,]$')
+ax[2].set_ylabel(r'$\Delta^2_{21,\delta}$')
+
+ax[2].text(25.5, 0.1, r'$k = 0.082\,\text{Mpc}^{-1}$')
 
 ax[0].legend(title='model', frameon=False)
 fig.tight_layout()
