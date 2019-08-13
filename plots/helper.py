505df3a33fcbc579424d7283686e65a88ba88b30
gusbeane <abeane@sas.upenn.edu>
2019-08-13 17:24:07 -0400

    Add Halpha intensity to helper.py

--- plots/helper.py
+++ plots/helper.py
@@ -4,6 +4,14 @@ import re
 from scipy.interpolate import interp1d
 from scipy.optimize import ridder
 
+import astropy.units as u
+from astropy.constants import c
+
+from colossus.cosmology import cosmology
+
+params = {'flat': True, 'H0': 67.66, 'Om0': 0.3111, 'Ob0': 0.02242/(0.6766**2), 'sigma8': 0.8102, 'ns': 0.9665}
+cosmo = cosmology.setCosmology('myCosmo', params)
+
 h = 0.6766
 
 def read_xps(files):
@@ -92,3 +100,19 @@ def compute_power_spectrum(field, Lbox, npix, kmin=None, kmax=None, deltak=1.4):
     Pklist = np.histogram(kmag_1d, bins, weights=np.square(np.absolute(field_ft_1d))/volume)[0]/Nlist
 
     return klist, Pklist
+
+def Halpha_intensity(z):
+    def psi(zp):
+        ans = 0.015
+        ans *= (1+zp)**(2.7)
+        ans /= 1 + ((1+zp)/2.9)**(5.6)
+        return ans
+
+    psi_at_z = psi(z) * u.Msun/u.year/(u.Mpc**3)
+    L0 = 3.29E7 * u.Lsun / (u.Msun/u.year)
+    Hz = cosmo.Hz(z) * u.km/u.s/u.Mpc
+    nurest_Ha = c/(656.28 * u.nm)
+
+    ans = (L0*psi_at_z / (4.*np.pi*nurest_Ha)) * (c/Hz)
+    return ans
+
